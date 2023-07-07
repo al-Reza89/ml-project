f3f159839793a5397ba0f866655d8bc79e9df6cb
al-Reza89 <alialreza98@gmail.com>
2023-07-07 15:38:09 +0600

    some loop issue

--- rokomari-dataset/rokomari/rokomari/spiders/bookspider.py
+++ rokomari-dataset/rokomari/rokomari/spiders/bookspider.py
@@ -36,9 +36,7 @@ class BookspiderSpider(scrapy.Spider):
             singleBookUrl=allbook.css('a').attrib['href']
             singleBookRedirectUrl='https://www.rokomari.com/'+singleBookUrl
             print(singleBookUrl)
-            yield{
-                'bookName': singleBookUrl
-            }
+            yield response.follow(singleBookRedirectUrl,callback=self.collectData)
 
         nextPageBookList=response.css('.pagination a::attr(href)').get()
 
@@ -46,3 +44,56 @@ class BookspiderSpider(scrapy.Spider):
             SingleAuthorNextPageBook='http://rokomari.com'+nextPageBookList
             yield response.follow(SingleAuthorNextPageBook,callback=self.parse)
 
+
+    def collectData(self,response):
+        # table_rows=response.css('table tr')
+        # try:
+        #     numberOfRating = int(response.css('.details-book-info__content-rating span::text').get().strip().split()[0])
+        # except (AttributeError, ValueError):
+        #     numberOfRating = None
+
+        
+
+        # try:
+        #     numberOfReview=int(response.css('.details-book-info__content-rating span a::text').get().strip().split()[0])
+        # except (AttributeError,ValueError):
+        #     numberOfReview=None
+
+        
+        # try:
+        #     summery=response.css('.tab-content .details-book-additional-info__content-summery .summary-description ::text').get()
+        # except (AttributeError,ValueError):
+        #     summery=""
+
+        # try:
+        #     orginalPrice=int(response.css('.details-book-info__content-book-price strike::text').get().strip().split()[1])
+        # except (AttributeError,ValueError):
+        #     orginalPrice=0
+
+        # try:
+        #     sellPrice=int(response.css('.details-book-info__content-book-price span::text').get().strip().split()[1])
+        # except (AttributeError,ValueError):
+        #     sellPrice=0
+
+
+        yield {
+            "url": response.url,
+            # "title": table_rows[0].css('td::text').getall()[1],
+            # "author": table_rows[1].css('td a::text').get(),
+            # "publisher": table_rows[2].css('td  a::text').get(),
+            # "isbn": table_rows[3].css('td::text').getall()[1],
+            # "page" : table_rows[4].css('td::text').getall()[1],
+            # "country": table_rows[5].css('td::text').getall()[1],
+            # "language" :table_rows[6].css('td::text').getall()[1],
+            # "category" : response.css(".details-book-info__content-category a::text").get(),
+            # "rating":"",
+            # "numberOfRating": numberOfRating,
+            # "numberOfReview" : numberOfReview,
+            # "orginalPrice" :  orginalPrice,
+            # "sellPrice" :sellPrice,
+            # "summery": summery,
+            # "imageUrl" :response.css('.look-inside::attr(src)').get(),
+
+        }
+
+        
